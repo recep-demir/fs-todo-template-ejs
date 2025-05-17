f08b0895dcc5e7d1275dd561a2ec192a063ef7cd
recep-demir <demir.rp@gmail.com>
2025-05-17 22:30:08 +0200

    feat: add JSON parsing middleware and start server in app.js

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
