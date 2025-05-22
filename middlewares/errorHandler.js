ec17492da252e334c833f63292973c0d35f4e50a
recep-demir <demir.rp@gmail.com>
2025-05-22 18:50:04 +0200

    feat: enhance errorHandler middleware to return structured error responses

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,20 @@
 "use strict";
 
 module.exports = (err, req, res, next) => {
+    const errorStatusCode = res.errorStatusCode ?? 500
+    console.log('errorHandler worked.')
+
+    const data = {
+        error: true, // special data
+        message: err.message, // error string message
+        cause: err.cause, // error option cause
+        // stack: err.stack, // error details
+    }
+
+    if (req.originalUrl.startsWith('/api')) {
+        res.status(errorStatusCode).send(data)
+    } else {
+        res.render('errors', { data })
+    }
     
 }
